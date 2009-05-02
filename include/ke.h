5912ed45aed20a056ba7c4f3bf1cf0f9fcd2deb4
sam.eskandari <sam.eskandari@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-02 02:49:32 +0000

    ke.h is now updated by Sam

--- include/ke.h
+++ include/ke.h
@@ -17,4 +17,29 @@ KeYieldProcessor(VOID);
 
 extern PPROCESS KeCurrentProcess;
 
+#ifndef __QUEUE__
+#define __QUEUE__
+
+#define MAXIMUM_QUEUE_LEVEL 32
+
+typedef struct _QUEUE
+{
+	PPROCESS First;
+	PPROCESS Last;
+} QUEUE, *PQUEUE;
+QUEUE priorities [MAXIMUM_QUEUE_LEVEL];
+
+VOID 
+KeEnqueue(PPROCESS object);
+
+VOID 
+KeDequeue(PPROCESS object);
+
+VOID 
+KeChangeProcessPriority(PPROCESS object, ULONG newPriority);
+
+STATUS 
+ProcSleep(PHANDLE PsHandle, ULONG Time);
+
+#endif //QUEUE
 #endif
