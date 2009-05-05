945d61cc862813c97d0032179d1c9d5c328f23a4
magnus.soderling <magnus.soderling@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-05 08:22:39 +0000

    Prereleas PCB done now with ps.h file included in trunk

---
+++ include/ps.h
@@ -0,0 +1,47 @@
+/* 
+ * File:   ps.h
+ * Author: Magnus Soderling
+ *
+ * Created on den 4 maj 2009, 22:44
+ */
+#ifndef _PS_H
+#define	_PS_H
+
+#include <types.h>
+#include <status.h>
+//#include <io.h> Not implemented
+
+//Defines possible states of a process.
+typedef enum {
+    running, ready, waiting, done
+}PROCESS_STATUS, *PPROCESS_STATUS;
+
+
+typedef struct _HANDLE_TABLE_ENTRY {
+    ULONG Attributes;
+    PVOID Object;
+} HANDLE_TABLE_ENTRY, *PHANDLE_TABLE_ENTRY;
+
+//
+// specifies the maximum number of open handles in a process
+//
+#define MAX_HANDLE_COUNT 256
+
+typedef struct _PROCESS {
+    CONTEXT Context;
+    HANDLE_TABLE_ENTRY HandleTable[MAX_HANDLE_COUNT];
+    PROCESS_STATUS ProcessStatus;
+    ULONG Priority;
+    PVOID ProgramCounter;
+    PVOID StackCounter;
+    PVOID AllocatedMemory;//Assume only one allocation per process
+    ULONG CPUTime;
+    // PMESSAGEQUEUE MessageQueue; MessageQUEUE not in any header
+    struct _PROCESS *NextPCB;
+} PROCESS, *PPROCESS;
+
+
+
+
+#endif	/* _PS_H */
+
