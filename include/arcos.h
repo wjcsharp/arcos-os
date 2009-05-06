9aa4a1d4cb2b10fc4d90baf989640d3404c46ef3
migeel <migeel@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-06 18:22:11 +0000

    Moved PROCESS back to ke.h and cleaned up the mess introduced by moving it to ps.h.

--- include/arcos.h
+++ include/arcos.h
@@ -131,4 +131,39 @@ typedef struct _CONTEXT {
 
 #endif
 
+/* PROCESS CONTROL BLOCK
+ * PROCESS and datastructures of PROCESS
+ */
+//Defines possible states of a process.
+typedef enum {
+    running, ready, waiting, done
+}PROCESS_STATUS, *PPROCESS_STATUS;
+
+
+typedef struct _HANDLE_TABLE_ENTRY {
+    ULONG Attributes;
+    PVOID Object;
+} HANDLE_TABLE_ENTRY, *PHANDLE_TABLE_ENTRY;
+
+//
+// specifies the maximum number of open handles in a process
+//
+#define MAX_HANDLE_COUNT 256
+
+typedef struct _PROCESS {
+    CONTEXT Context;
+    HANDLE_TABLE_ENTRY HandleTable[MAX_HANDLE_COUNT];
+    PROCESS_STATUS ProcessStatus;
+    ULONG Priority;
+    PVOID ProgramCounter;
+    PVOID StackCounter;
+    PVOID AllocatedMemory;//Assume only one allocation per process
+    ULONG CPUTime;
+    // PMESSAGEQUEUE MessageQueue; MessageQUEUE not in any header
+    // PMESSAGEQUEUE Supervisor;
+    struct _PROCESS *NextPCB;
+} PROCESS, *PPROCESS;
+
+
+
 #endif
--- include/ps.h
+++ include/ps.h
@@ -1,53 +1,19 @@
-/*
+/* 
  * File:   ps.h
  * Author: Magnus Soderling
  *
  * Created on den 4 maj 2009, 22:44
  */
-
-#ifndef __PS_H__
-#define	__PS_H__
+#ifndef _PS_H
+#define	_PS_H
 
 #include <arcos.h>
-/* Following includes generates build error
-#include <ob.h>
 #include <mm.h>
-*/
+//#include <io.h> Not implemented
 
 
-/* PROCESS CONTROL BLOCK
- * PROCESS and datastructures of PROCESS
- */
-
-/* Defines possible states of a process.*/
-typedef enum {
-    running, ready, waiting, done
-}PROCESS_STATUS, *PPROCESS_STATUS;
 
 
-typedef struct _HANDLE_TABLE_ENTRY {
-    ULONG Attributes;
-    PVOID Object;
-} HANDLE_TABLE_ENTRY, *PHANDLE_TABLE_ENTRY;
 
-//
-// specifies the maximum number of open handles in a process
-//
-#define MAX_HANDLE_COUNT 256
-
-typedef struct _PROCESS {
-    CONTEXT Context;
-    HANDLE_TABLE_ENTRY HandleTable[MAX_HANDLE_COUNT];
-    PROCESS_STATUS ProcessStatus;
-    ULONG Priority;
-    PVOID ProgramCounter;
-    PVOID StackCounter;
-    PVOID AllocatedMemory;//Assume only one allocation per process
-    ULONG CPUTime;
-    /* PMESSAGEQUEUE MessageQueue; MessageQUEUE not in any header */
-    /* PMESSAGEQUEUE Supervisor; */
-    struct _PROCESS *NextPCB;
-} PROCESS, *PPROCESS;
-
-#endif	// __PS_H__ 
+#endif	/* _PS_H */
 
