9ef8a8d3013a8e4ca7a7a3928320936a19be8a8b
magnus.soderling <magnus.soderling@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-11 14:41:30 +0000

    Change exitstatus to ULONG in _PROCESS more create process...

--- base/ps/ps.c
+++ base/ps/ps.c
@@ -10,32 +10,70 @@
 #include <ob.h>
 
 
+//Create Process object type
+
+STATUS
+CreateProcessObjectType(
+        POBJECT_TYPE ProcessType
+        ) {
+    OBJECT_TYPE_INITIALIZER typeInitializer;
+    STATUS status;
+
+    //
+    // create a new object type: Process
+    //
+    typeInitializer.DumpMethod = NULL; //Should be implemented...
+    typeInitializer.DeleteMethod = NULL;
+    status = ObCreateObjectType('proc', &typeInitializer, &ProcessType);
+
+    return status;
+}
+
+
+
 STATUS
 PsCreateProcess(
         PVOID (*PStartingAddress)(),
         ULONG Priority,
         PHANDLE ProcessHandle,
         PCHAR Args
-        ){
+        )
+{
     STATUS status;
-    PVOID mempointer;
-    PPROCESS newprocess = (PPROCESS)ProcessHandle;
+    PVOID createdProcessObject=NULL;
+    PPROCESS process;
+    ///-------SHOULD Be moved to initialize only once
+    POBJECT_TYPE processType = NULL;
+    status = CreateProcessObjectType(processType);
+    //----------------------------------------------------
 
-
-//Ask object manager for a new processobject
-    status = ObInitProcess(NULL, newprocess);
-    if (status != STATUS_SUCCESS)
-        return status;
-
-
-    newprocess->Priority = Priority;
-
+    status = ObCreateObject(processType, 0, sizeof(PROCESS), createdProcessObject);
+    process = (PPROCESS)createdProcessObject;
+    process->Priority = Priority;
+    process->CPUTime = 0;
     
-//Ask mamory manager for a chunk of memory to be attached to the process
-    mempointer = MmAlloc(PROCESS_MEMORY_TO_ALLOCATE);
-    if (mempointer == NULL)
-        return STATUS_NO_MEMORY;
-  
-    ProcessHandle = (PHANDLE)newprocess;
-    return STATUS_SUCCESS;
+    return status;
 }
+
+
+
+
+/*
+//Ask object manager for a new processobject
+
+
+
+
+
+
+//Ask mamory manager for a chunk of memory to be attached to the process
+mempointer = MmAlloc(PROCESS_MEMORY_TO_ALLOCATE);
+if (mempointer == NULL)
+    return STATUS_NO_MEMORY;
+
+
+return STATUS_SUCCESS;
+
+
+
+*/
