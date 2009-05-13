36c388157a42fc6a885e9ce1c551b5fd1fb79ab8
hugo.heyman <hugo.heyman@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-13 13:32:58 +0000

    MmAlloc and MmInitialize reworked

--- base/mm/mm.c
+++ base/mm/mm.c
@@ -1,15 +1,23 @@
 /*
         Module: mm.c
-        Author: Olle Harstedt
+        Author: Hugo Heyman
 
+
+		Please comment on the MmAlloc.
+		I know the block list is only single linked so far, I'll add PreviousBlock refering when making the Free
+		
 */
 
-#include <arcos.h>      // Which one are necessary here ??
-//#include <ke.h>
+#include <arcos.h> 
 #include <mm.h>
 #include <hal.h>        // For HalGetFirstUsableMemoryAdress().
 
-PVOID freeMemPointer;  // The pointer to the free adress space.
+
+// First free memory address
+PVOID FirstMemPointer; 
+
+// Memory Block list
+PMEMORY_BLOCK MemBlock;
 
 //
 // aligns memory pointer at a 4-byte boundary
@@ -19,22 +27,78 @@ PVOID freeMemPointer;  // The pointer to the free adress space.
 VOID
 MmInitialize()
 {
-        freeMemPointer = HalGetFirstUsableMemoryAddress();    // Correct? A lot of warnings here...
+	FirstMemPointer = HalGetFirstUsableMemoryAddress();
+	MemBlock = ALIGN_MEMORY((ULONG)MemBlock + FirstMemPointer + MAIN_MEM_SIZE);
+	MemBlock->NextBlock = NULL;
+	MemBlock->PreviousBlock = NULL;
+	MemBlock->IsFree = 1; 
 }
 
+
+/*
 PULONG
 MmGetMemPointer(){
         return freeMemPointer;
 }
+*/
 
-PVOID
-MmAlloc(
-        ULONG size      // Could we use parameter "type" here instead?
-        )
-{
-        PVOID returnPointer = freeMemPointer;                   // Give current address to object
-        freeMemPointer = ALIGN_MEMORY((ULONG)freeMemPointer + size); // Increase freeMemPointer	
+PVOID MmAlloc(ULONG SizeToBeAllocated) {
+	
+	// Size of the header
+	ULONG HeaderSize = sizeof(PMEMORY_BLOCK);
+	
+	// Copy of the mb-list
+	PMEMORY_BLOCK PMb = MemBlock;
+	
+	// Search for a free block to use
+	while (PMb != FirstMemPointer) {
+		// Block big enough and free?
+		if(PMb->Size > SizeToBeAllocated + HeaderSize && PMb->IsFree == 1) {
+			ULONG FragmentSize = PMb->Size - (SizeToBeAllocated + HeaderSize);
+			// Is the fragment block bigger than the header?
+			if (FragmentSize > HeaderSize) {
+				// Alloc the requested block
+				PMb = ALIGN_MEMORY((ULONG)PMb + SizeToBeAllocated + HeaderSize);
+
+				// Create and alloc the fragment block
+				PMEMORY_BLOCK FragmentMb;
+				FragmentMb= ALIGN_MEMORY((ULONG)PMb + FragmentSize);
+				FragmentMb->NextBlock = PMb->NextBlock;
+				FragmentMb->IsFree = 1;
+
+				// Put the fragment block in the list and make it free
+				PMb->NextBlock = FragmentMb;
+				PMb->IsFree = 0;
+				return PMb;
+			}
+			// ..otherwise alloc the full block (make no fragment block)
+			else {
+				PMb = ALIGN_MEMORY((ULONG)PMb + SizeToBeAllocated + HeaderSize + FragmentSize);
+				PMb->IsFree = 0;
+				return PMb;
+			}
+		}
+		// Check the next block
+		else {
+			PMb = PMb->NextBlock;
+		}
+	}
+	
+	// No free block was found so a new one is created
+	PMb = ALIGN_MEMORY((ULONG)PMb + SizeToBeAllocated + HeaderSize);
+	PMb->IsFree = 0;
+	
+	
+	PMb->NextBlock = MemBlock;
+	MemBlock = PMb;
+	return PMb;
+	
+	// Old code
+	/*
+    PVOID returnPointer = freeMemPointer;                   // Give current address to object
+	freeMemPointer = ALIGN_MEMORY((ULONG)freeMemPointer + size); // Increase freeMemPointer	
 	return returnPointer;
+	*/
 }
 
 VOID
