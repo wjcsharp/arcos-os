35a2cd4358b0aa45806e267bffeb5453974aef24
olleharstedt <olleharstedt@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-18 18:35:25 +0000

    New message code.

--- base/mess/mess.c
+++ base/mess/mess.c
@@ -3,6 +3,7 @@
 #include <mess.h>
 #include <rtl.h>
 #include <ps.h>
+#include <ke.h>
 
 STATUS
 MessSend(
@@ -45,11 +46,6 @@ MessSend(
 	return STATUS_SUCCESS;
 }
 
-/*************************
-
-Magnus wrote:
-I will need a dealloc message queue function that frees every message in the list and then sets the list pointer to null
-This is to free memory when a process is killed.
 
 
 PVOID
@@ -57,20 +53,45 @@ MessReceiveFirst(
     ULONG timeout
     )
 {
+
+	PMESSAGE newMessage;
+
+	if (KeCurrentProcess->MessageQueue != NULL)
+	{
+		newMessage = KeCurrentProcess->MessageQueue;
+		newMessage->next = NULL;		// No tail with messages on new message.
+		KeCurrentProcess->MessageQueue = KeCurrentProcess->MessageQueue->next;	// Jump forward one step in the queue, skipping the first one.
+		return newMessage;	// Return pointer to new message. Should be copied by the process, I guess, and then deleted.
+	}		
+	else	// No message in message queue - wait a while/timeout.
+	{
+		KeSuspendProcess(timeout);
+		return NULL;
+	}
+
+	//KdPrint("MESS: Message queue in current process is NULL");
+
+	/*
     if (message is available in the msg list) {
         return (PVOID)message;
     }
     remember that this process is waiting for a message;
     // stops scheduling the process until the timeout expires
-    KeSuspendProcess(timeout);
     // when the timeout expires, NULL will be returned.
     // but if someone sends a message before the timeout expires,
     // KeSetSyscallResult in MessSend will overwite this value before
     // the syscall returns in the resumed process
     return NULL;
+	*/
 }
 
  
+/*************************
+
+Magnus wrote:
+I will need a dealloc message queue function that frees every message in the list and then sets the list pointer to null
+This is to free memory when a process is killed.
+
 
 ULONG
 
