9da8c4e4de89a495c5cd63e15824210deb145b38
sam.eskandari <sam.eskandari@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-21 23:23:53 +0000

    shell added to ARCOS.

---
+++ trunk/base/apps/shell.c
@@ -0,0 +1,36 @@
+/*
+ *  s1.c
+ *
+ *  Created on: May 21, 2009
+ *  Author: Sam Eskandari
+ */
+
+#include <stdio.h>
+
+void split(char *string,char *pCommand,char *pArgument)
+{
+	pCommand = string;
+	int i;
+	for(i=0;string[i] != '\0';i++)
+	{
+		if(string[i] == ' ')
+		{
+			string[i] = '\0';
+			pArgument = &string[i+1];
+			break;
+		}
+	}
+	printf("\nCommand is: %s",pCommand); //delete it
+	printf("\nArgument is %s",pArgument); //delete it
+}
+
+int main()
+{
+	char input[256]; //buffer
+	char *command;
+	char *argument;
+	printf("\nWrite a string: "); //delete it
+	gets(input); //reads from keyboard
+	split(input,command,argument);
+	return 0;
+}
