257adc1ada3f89b9271eb914c92834ebe5e6c58c
sam.eskandari <sam.eskandari@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-24 09:35:31 +0000

    Working shell. We are looking for bugs. It's not completely bug free

--- base/apps/shell.c
+++ base/apps/shell.c
@@ -5,12 +5,20 @@
  *  Author: Sam Eskandari
  */
 
-#include <stdio.h>
+#include <io.h>
+#include <arcos.h>
+#include <ps.h>
+#include <kd.h>
+#include <api.h>
+#include <rtl.h>
 
 void split(char *string,char *pCommand,char *pArgument)
 {
+	ULONG i;
+	HANDLE handle;
+	STATUS status;
+	handle = CreateFile('s');
 	pCommand = string;
-	int i;
 	for(i=0;string[i] != '\0';i++)
 	{
 		if(string[i] == ' ')
@@ -19,18 +27,73 @@ void split(char *string,char *pCommand,char *pArgument)
 			pArgument = &string[i+1];
 			break;
 		}
+		else
+			pArgument = "";
 	}
-	printf("\nCommand is: %s",pCommand); //delete it
-	printf("\nArgument is %s",pArgument); //delete it
+	i = 0;
+	while(pCommand[i])
+	{
+		KdPrint("%c",pCommand[i]);
+		i++;
+		if(pCommand[i] == NULL)
+			KdPrint("%s","NULLLLLLLLLLLLLLLLLLLLLLL");
+	}
+	KdPrint("Command:%s",pCommand);
+	HANDLE commandProcess;
+	KdPrint("Status is: %d",status);
+	KdPrint("Argument is:%s11",pArgument);
+	KdPrint("Argument Length is: %d",RtlStringLength(pArgument));
+	status = CreateProcess(pCommand,31,&commandProcess,pArgument);
+	KdPrint("Length is: %d",RtlStringLength(pCommand));
+	//PsCreateProcessByName("TestProcess3", 1, &testProcess3, NULL);
+    //ObCloseHandle(testProcess3);
+	
 }
 
-int main()
+void shell()
 {
-	char input[256]; //buffer
-	char *command;
-	char *argument;
-	printf("\nWrite a string: "); //delete it
-	gets(input); //reads from keyboard
-	split(input,command,argument);
-	return 0;
+	CHAR input[100]; //buffer
+	PCHAR c;
+	CHAR ch[2];
+	PCHAR command;
+	PCHAR argument;
+	HANDLE handle;
+	ULONG i;
+	ULONG n;
+	ch[1] = NULL;
+	c = ch;
+	handle = CreateFile('s');
+	WriteFile(handle,"ARCOS:\\>",0);
+		i = 0;
+		while(*c != '\r')
+		{
+			command = NULL;
+			argument = NULL;
+			ReadFile(handle,c,1);
+			if (*c == '\b' || *c == 127) 
+			{
+				if (i > 0) 
+				{
+					i = i-1;
+					input[i]='\0';
+					WriteFile(handle,"\e[D \e[D",0);
+				}
+			}
+			else if(*c == '\r')
+				break;
+			else
+			{
+				input[i] = ch[0];
+				WriteFile(handle,c,1);
+				i++;
+			}
+			if(i>99)
+				i = 0;
+		}
+		split(input,command,argument);
+		for(n=0;n<i;n++)
+			input[n] = '\0';
+		ch[0] = '\0';
+		WriteFile(handle,"\n",0);
+	//}
 }
