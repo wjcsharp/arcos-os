f822f3a8e25ca009bb630db6bb8ce21cf9923f69
hugo.heyman <hugo.heyman@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-25 16:22:53 +0000

    philosopher.c wasn't uploaded, trying again

---
+++ trunk/base/apps/philosopher.c
@@ -0,0 +1,120 @@
+/****************************************
+	philosopher.c
+
+	author: Hugo Heyman, 2009-05-25
+
+	Philosopher node process handling
+
+*****************************************/
+
+
+#include <arcos.h>
+#include <rtl.h>
+#include <api.h>
+#include <kd.h>
+
+
+VOID AppPhilosopher() {
+	
+	ULONG LeftForkNum, RightForkNum, WaiterPID;
+	PCHAR LeftForkMess, RightForkMess, LeftDropMess, RightDropMess;
+
+	PVOID NewMess;
+
+	// Get args from waiter process
+	PCHAR WaiterArgs = KeCurrentProcess->Args;
+
+	// Make separate ULONGs from the WaiterArgs string
+	//
+	LeftForkNum = (ULONG)WaiterArgs;
+	WaiterArgs++;
+	WaiterArgs++;
+	RightForkNum = (ULONG)WaiterArgs;
+	WaiterArgs++;
+	WaiterArgs++;
+
+	WaiterPID = RtlAtoUL(WaiterArgs);
+
+
+	// Some debugging
+
+	KdPrint("LeftFork: %d", LeftForkNum);
+
+	KdPrint("RightFork: %d", RightForkNum);
+
+	KdPrint("WaiterPID: %d", WaiterPID);
+
+
+	// Make left fork message
+	LeftForkMess = (PCHAR)'L';
+	LeftForkMess++;
+	LeftForkMess = (PCHAR)LeftForkNum;
+	LeftForkMess++;
+	LeftForkMess = '\0';
+
+	// Make right fork message
+	RightForkMess = (PCHAR)'R';
+	RightForkMess++;
+	RightForkMess = (PCHAR)RightForkNum;
+	RightForkMess++;
+	RightForkMess = '\0';
+	
+	// Make left fork drop message
+	LeftDropMess = (PCHAR)'D';
+	LeftDropMess++;
+	LeftDropMess = (PCHAR)LeftForkNum;
+	LeftDropMess++;
+	LeftDropMess = '\0';
+
+	// Make left fork drop message
+	RightDropMess = (PCHAR)'D';
+	RightDropMess++;
+	RightDropMess = (PCHAR)RightForkNum;
+	RightDropMess++;
+	RightDropMess = '\0';
+
+	/*********** Philosopher loop ******************/
+	//
+	while(1) {
+
+
+		// Think
+		Sleep(2000);
+
+
+		// Get first chopstick
+			SendMessage(WaiterPID, 0, LeftForkMess, 3);
+			NewMess = NULL;
+			while(!NewMess) {
+				NewMess = ReceiveFirst(1000);
+			}
+			DeleteMessage(NewMess);
+
+
+		// Get second chopstick
+			SendMessage(WaiterPID, 0, RightForkMess, 3);
+
+			NewMess = NULL;
+			while(!NewMess) {
+				ReceiveFirst(1000);
+			}
+			DeleteMessage(NewMess);
+
+
+		// Eat
+		Sleep(3000);
+
+
+		// Drop left chopstick
+		SendMessage(WaiterPID, 0, LeftDropMess, 3);
+
+
+		// Drop right chopstick
+		SendMessage(WaiterPID, 0, RightDropMess, 3);
+
+	}
+	
+	/************************************************/
+	
+
+}
