de0a008a926a4992c015a104c35ca10fc4a3ea82
sam.eskandari <sam.eskandari@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-26 13:28:59 +0000

    Add Exit command to kill the shell (again), remove ObCloseHandle and use just one handle.

--- trunk/base/apps/shell.c
+++ trunk/base/apps/shell.c
@@ -7,12 +7,19 @@
 
 #include <kd.h>
 #include <api.h>
+#include <arcos.h>
+#include <apps.h>
+#include <rtl.h>
 
-void split(char *string, char **pCommand, char **pArgument) {
+
+void split(char *string, char **pCommand, char **pArgument) 
+{
     ULONG i;
     *pCommand = string;
-    for (i = 0; string[i] != '\0'; i++) {
-        if (string[i] == ' ') {
+    for (i = 0; string[i] != '\0'; i++) 
+	{
+        if (string[i] == ' ') 
+		{
             string[i] = '\0';
             *pArgument = &string[i+1];
             break;
@@ -21,54 +28,57 @@ void split(char *string, char **pCommand, char **pArgument) {
     }
 }
 
-void AppShell() {
+void AppShell() 
+{
     CHAR input[100], ch[2]; 
     PCHAR c, command, argument;
-    HANDLE serialinout, outp, commandProcess;
-    ULONG i, n;
+    HANDLE handle, commandProcess;
+	ULONG i, n;
     STATUS status;
-    outp = CreateFile('s');
-	serialinout = CreateFile('s');
+    handle = CreateFile('s');
     ch[1] = NULL;
     c = ch;
-    WriteFile(serialinout, "ARCOS:\\>", 9);
+    WriteFile(handle, "ARCOS:\\>", 9); 
     i = 0;
-    while (*c != '\r') {
+    while (*c != '\r') 
+	{
         command = NULL;
         argument = NULL;
-        ReadFile(serialinout, c, 1);
-        if (*c == '\b' || *c == 127) {
-            if (i > 0) {
+        ReadFile(handle, c, 1); 
+        if (*c == '\b' || *c == 127) 
+		{
+            if (i > 0) 
+			{
                 i = i - 1;
                 input[i] = '\0';
-                WriteFile(serialinout, "\e[D \e[D", 9);
+                WriteFile(handle, "\e[D \e[D", 9); 
             }
-        } else if (*c == '\r')
+        } 
+		else if (*c == '\r')
             break;
-        else {
+        else 
+		{
             input[i] = ch[0];
-            WriteFile(serialinout, c, 1); //BUGBUGBUG?
+            WriteFile(handle, c, 1);  
             i++;
         }
         if (i > 99)
             i = 0;
     }
     split(input, &command, &argument);
-		
-	status = CreateProcess(command, 9, &commandProcess, argument);
-    if (status != 0) {
-        WriteFile(outp, "\n\rARCOS:\\>Unknown command. Try again.", 38);
-        ObCloseHandle(outp);
-    }
-
-    if (status == 0) {
-        ASSERT(commandProcess);
-        ObCloseHandle(commandProcess);
-    }
-    ObCloseHandle(outp);
-    for (n = 0; n < i; n++)
-        input[n] = '\0';
-    ch[0] = '\0';
-    WriteFile(serialinout, "\n\r", 1);
-    ObCloseHandle(serialinout);
+	if (RtlCompareStrings(command,"Exit") == 0)
+		KillMe();
+	else
+	{
+		status = CreateProcess(command, 9, &commandProcess, argument);
+		if (status != 0) 
+		{
+			WriteFile(handle, "\n\rARCOS:\\>Unknown command. Try again.", 38); 
+		}
+		for (n = 0; n < i; n++)
+			input[n] = '\0';
+		ch[0] = '\0';
+		WriteFile(handle, "\n\r", 1); 
+	}
 }
+
