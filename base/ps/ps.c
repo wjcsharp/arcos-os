c12374fa7958662323f53abb18b0a331324bcc70
magnus.soderling <magnus.soderling@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-05-26 10:21:33 +0000

    Kill in its own file

--- base/ps/ps.c
+++ base/ps/ps.c
@@ -95,32 +95,6 @@ AppChangePrio() {
     KillMe();
 }
 
-VOID
-AppKill() {
-    ULONG PID, argLength;
-    STATUS status;
-    CHAR Args[25];
-    //Get function args
-    argLength = CopyArgs(Args, 25);
-    KdPrint(":::%s:::%d", Args, argLength);
-    if (argLength == 0) {
-        KdPrint("Kill needs an argument e.g. 'kill 5'"); //BUGBUGBUG
-        KillMe();
-    }
-
-    PID = RtlAtoUL(Args);
-
-    status = KillByPID(PID, 1); //ExitStatus 1 == murdered
-    if (0 == status)
-        KdPrint("Killed %d", PID); //BUGBUGBUG
-    else
-        KdPrint("Failed to kill %d", PID); //BUGBUGBUG
-
-    KillMe();
-}
-
-
-
 
 
 VOID
