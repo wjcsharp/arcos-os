93704410323b88200f3cb4a1bf685c24cd1ed594
magnus.soderling <magnus.soderling@78753a56-1f05-11de-b650-e715bd6d7cf1>
2009-06-04 10:54:57 +0000

    keep alive still crashes dont know why

--- trunk/base/apps/drone.c
+++ trunk/base/apps/drone.c
@@ -8,7 +8,7 @@
 
 VOID
 AppDrone() {
-    ULONG i;
+    ULONG i, j;
     HANDLE outp;
     CHAR outstring[80];
 
@@ -17,14 +17,15 @@ AppDrone() {
     RtlFormatString(outstring, 80, "\r\nDrone with PID:%d started\r\n", GetProcessId());
     WriteString(outp, outstring);
     KdPrint("in drone 1");
-    Sleep(DRONE_SLEEP_TIME);
-
+    //Sleep(DRONE_SLEEP_TIME);
+    KdPrint("in drone after sleep");
     RtlFormatString(outstring, 80, "\r\nDrone with PID:%d still alive\r\n", GetProcessId());
 
     for (i = 0; i < 3; i++) {
         KdPrint("in drone 2");
         WriteString(outp, outstring);
-        Sleep(DRONE_SLEEP_TIME);
+        for(j=0;j<0xFFFFFF;j++);
+        //Sleep(DRONE_SLEEP_TIME);
     }
     RtlFormatString(outstring, 80, "\r\nDrone with PID:%d killing my self\r\n", GetProcessId());
     KdPrint("in drone 3");
